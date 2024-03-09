d820eea21f3d2404e742c8bf95b018927978dbcb
bhakti0509 <bhaktisatpute05@gmail.com>
2024-03-09 14:11:15 +0530

    UI

--- src/loginform.py
+++ src/loginform.py
@@ -123,7 +123,7 @@ class LoginPage:
 
         self.TB_left_frame = tk.Frame(self.new_window, bg="#00072D",width=self.TB_screen_width // 6.3, height=self.TB_screen_height)
         self.TB_left_frame.pack(side=tk.LEFT)
-
+    
         script_directory = os.path.dirname(os.path.abspath(__file__))
         img_path = os.path.join(script_directory, "../assets/TBLogoFinal.png")
         img = ImageTk.PhotoImage(Image.open(img_path))
@@ -132,16 +132,42 @@ class LoginPage:
         self.logo.pack()
         self.logo.place(x=0, y=0, anchor="nw")
 
+        img2_path = os.path.join(script_directory,"../assets/pune-darshan.jpg")
+        img2 = ImageTk.PhotoImage(Image.open(img2_path))
+        self.img2place = tk.Label(self.new_window,image= img2)
+        self.img2place.image = img2
+        self.img2place.pack()
+        self.img2place.place(x=1005,y=500, anchor= 'nw')
+
+        img3_path = os.path.join(script_directory,"../assets/img3Final.jpg")
+        img3 = ImageTk.PhotoImage(Image.open(img3_path))
+        self.img3place = tk.Label(self.new_window,image= img3)
+        self.img3place.image = img3
+        self.img3place.pack()
+        self.img3place.place(x=303.5,y=500, anchor= 'nw')
+
+        self.choose_destination_label = tk.Label(self.new_window, text="Choose a destination:",height=3,width=20,fg='Black',bg='Yellow')
+        self.choose_destination_label.config(font=('TimesNewRoman',25))
+        self.choose_destination_label.pack(padx=20,pady=20)
+
+        self.destination_options = ttk.Combobox(self.new_window, values=["By Price", "By Vibe", "By Location"],height=30,width=30)
+        self.destination_options.config(font=('TimesNewRoman',15))
+        self.destination_options.pack(padx=15,pady=15)
+
+        self.choose_button = ttk.Button(self.new_window, text="Choose", command=self.show_destination)
+        self.choose_button.pack()
 
 
-        #find a destination:
-        destFindBy = tk.Label(self.new_window,text='Find Distination By',bg="#000000",font='TimesNewRoman',fg='white')
-        destFindBy.pack(side='top',padx=60,pady=80)
-        self.destinations = ['Shaniwar Wada','Sinhgad Fort','Swaminarayan Temple','Panshet Dam','Rajgad Fort','Khadakwasla Dam','FC Road',
-                             'Lavasa City','Ramdara Temple','Lonavala']
-        Combo = ttk.Combobox(self.new_window, values = self.destinations)
-        Combo.set("Choose a Destination")
-        Combo.pack(padx = 100, pady = 50)
+    def show_destination(self):
+        option = self.destination_options.get()
+        if option == "By Price":
+            messagebox.showinfo("Destination", "You chose destination by Price!")
+            
+
+        elif option == "By Vibe":
+            messagebox.showinfo("Destination", "You chose destination by Vibe!")
+        elif option == "By Location":
+            messagebox.showinfo("Destination", "You chose destination by Location!")
 
     
     def run(self):
